8412c17e5f56eeaff5e60f42d6e2d5b5ced17e57
François Bellin <francois.bellin@vinci.com>
2021-02-08 22:15:58 +0100

    Adds postgres container and refactoring of Dockerfile and docker-compose organization.

--- app/src/users/data.sql
+++
@@ -1,3 +0,0 @@
-insert into users values
-(1, 'john@doo.com', 'johndoo'),
-(3, 'foo@bar.com', 'foobar')
---
+++ db/init.sql
@@ -0,0 +1,11 @@
+create table if not exists users (
+    id integer primary key,
+    name varchar(64) not null,
+    password varchar(64) not null
+)
+;
+
+insert into users values
+(1, 'john@doo.com', 'johndoo'),
+(3, 'foo@bar.com', 'foobar')
+;
